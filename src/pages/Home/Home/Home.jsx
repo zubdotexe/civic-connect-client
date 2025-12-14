76fceb60417fcde20b6a3fd16ff6c1fdb1d9db32
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-12-14 14:18:19 +0600

    added covered categories section to homepage

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Hero/Hero'
 import HowItWorks from '../HowItWorks/HowItWorks'
 import Features from '../Features/Features'
+import Benefits from '../Benefits/Benefits'
+import CoveredCategories from '../CoveredCategories/CoveredCategories'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
         <Hero />
         <Features />
         <HowItWorks />
+        <Benefits />
+        <CoveredCategories />
     </div>
   )
 }
