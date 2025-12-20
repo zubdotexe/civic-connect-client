90537c6cd4f12cea82938ac27b2538cf4b509ee5
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-12-20 23:14:47 +0600

    added auth context provider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthContextProvider from "./contexts/AuthContext/AuthContextProvider.jsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <AuthContextProvider>
+                <RouterProvider router={router} />
+            </AuthContextProvider>
         </QueryClientProvider>
     </StrictMode>
 );
