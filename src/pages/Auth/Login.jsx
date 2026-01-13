b7c4e02095aa20f25959d0a3b1f70aded44fe65e
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2026-01-13 20:34:44 +0600

    updated Login component

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import useAuth from "../../hooks/useAuth";
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
+import SocialLogin from "./SocialLogin";
+import { toast } from "react-toastify";
 
 export default function Login() {
     const {
@@ -9,17 +11,29 @@ export default function Login() {
         formState: { errors },
     } = useForm();
     const { signInUser } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const handleLogin = (data) => {
         console.log("", data);
-        signInUser(data.email, data.password);
+        signInUser(data.email, data.password)
+            .then((result) => {
+                console.log("", result);
+                navigate(location.state || "/");
+            })
+            .catch((err) => {
+                toast.error(err.message);
+            });
     };
 
     return (
-        <div className="h-screen flex justify-center items-center">
+        <div className="relative h-screen flex justify-center items-center bg-black/45">
+            <div className="absolute h-full w-full top-0 overflow-hidden -z-1">
+                <img className="h-full w-full object-cover" src="https://cdn.pixabay.com/photo/2016/11/19/13/16/infrastructure-1839235_1280.jpg" alt="" />
+            </div>
             <div>
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Login</legend>
+                    <legend className="fieldset-legend text-2xl">Login</legend>
 
                     <form action="" onSubmit={handleSubmit(handleLogin)}>
                         <label className="label">Email</label>
@@ -50,8 +64,9 @@ export default function Login() {
                             Login
                         </button>
                     </form>
+                    <SocialLogin phrase={"Login"} />
                 </fieldset>
-                <div className="mt-5 text-secondary text-center">
+                <div className="mt-5 text-accent text-center">
                     <Link to="/">Go to Home</Link>
                 </div>
             </div>
