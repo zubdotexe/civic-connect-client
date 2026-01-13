505297518d73a0eb14cc7e459b2575e07b1b1b16
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2026-01-13 22:07:31 +0600

    implemented marquee in CoveredCategories component

--- src/pages/Home/CoveredCategories/CoveredCategories.jsx
+++ src/pages/Home/CoveredCategories/CoveredCategories.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import Marquee from "react-fast-marquee";
 
 export default function CoveredCategories() {
     return (
-        <div className="max-w-375 mx-auto p-10 bg-base-200">
+        <div className="max-w-375 mx-auto p-10 bg-base-100">
             <h2 className="text-3xl font-semibold">Categories We Cover</h2>
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 mt-7">
+            {/* <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 mt-7">
                 <div className="bg-base-300 hover:bg-base-300/60 rounded-md p-5 text-center shadow-md">
                     <span className="text-rotate text-2xl">
                         <span className="justify-items-center">
@@ -41,6 +41,29 @@ export default function CoveredCategories() {
                         </span>
                     </span>
                 </div>
+            </div> */}
+
+            <div className="mt-9">
+                <Marquee className="py-3">
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>🛣️ Road & Potholes</span>
+                    </div>
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>💡 Street Lights</span>
+                    </div>
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>🚰 Water Leakage</span>
+                    </div>
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>🗑️ Garbage Overflow</span>
+                    </div>
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>🚧 Footpath Damage</span>
+                    </div>
+                    <div className="mx-4 bg-base-300 rounded-md p-5 w-72 text-center shadow-md">
+                        <span>🌳 Public Space Issues</span>
+                    </div>
+                </Marquee>
             </div>
         </div>
     );
